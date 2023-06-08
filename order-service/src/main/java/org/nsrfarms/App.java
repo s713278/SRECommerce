5f7f05a7ff9be859a111a1979f4cc077acc31f3b
K Swamy <swamy.kunta@gmail.com>
2023-06-08 07:35:55 +0530

    Initial Commit

---
+++ order-service/src/main/java/org/nsrfarms/App.java
@@ -0,0 +1,13 @@
+package org.nsrfarms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
