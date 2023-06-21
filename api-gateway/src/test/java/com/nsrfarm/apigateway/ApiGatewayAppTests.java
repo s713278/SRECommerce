f6c31adb15e0a730b08f2d8014f4e1807afb3cbc
K Swamy <swamy.kunta@gmail.com>
2023-06-21 18:04:21 +0530

    4. Added API Gateway Module working POC

---
+++ api-gateway/src/test/java/com/nsrfarm/apigateway/ApiGatewayAppTests.java
@@ -0,0 +1,13 @@
+package com.nsrfarm.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
