0b8c47352fee150009e38cde4265659cf4bae1a3
K Swamy <swamy.kunta@gmail.com>
2023-07-07 15:47:37 +0530

    Added bootstrap CSS in inex.html and CSS applied for component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
 import CatalogComponent from './component/CatalogComponent';
+import RowItem from './component/ToDoItemComponent';
 
 function App() {
   return (
-    <div className="App">
-      <CatalogComponent/>
-    </div>
+    <diV className="mt-5 container">
+      <div className="card">
+        <div className="card-header">
+          Your Todo's
+        </div>
+        <div className="card-body">
+          <table className="table table-hover">
+            <thead>
+              <tr>
+                <th scope='col'>#</th>
+                <th scope='col'>Desc</th>
+                <th scope='col'>Assign</th>  
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope='row'>1</th>
+                <td >Drop Chetan @School </td>
+                <td >Venkat</td>
+              </tr>
+              <tr>
+                <th scope='row'>1</th>
+                <td >Pickup Chetan from School </td>
+                <td >Venkat</td>
+              </tr>
+              <RowItem/>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </diV>
   );
 }
 
---
+++ front-end/src/component/ToDoItemComponent.js
@@ -0,0 +1,10 @@
+function RowItem() {
+ return(
+    <tr>
+        <th scope="row">3</th>
+        <td>SSS</td>
+        <td>ffff</td>
+    </tr>
+ )
+}
+export default RowItem;
